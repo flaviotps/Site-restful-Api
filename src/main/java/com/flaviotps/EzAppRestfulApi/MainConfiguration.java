bc6931cc11b48bb3c2b9e68190c612b978c6cc19
flaviotps <flaviotps@gmail.com>
2018-10-07 20:59:00 -0300

    Commit Inicial

--- src/main/java/com/flaviotps/EzAppRestfulApi/MainConfiguration.java
+++ src/main/java/com/flaviotps/EzAppRestfulApi/MainConfiguration.java
@@ -13,7 +13,7 @@ public class MainConfiguration {
 
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI("");
+        URI dbUri = new URI("postgres://eipnejvbbzxlug:7db22778ca0da5ef872fb24c8ab9ae3fd2279a485d0f675cdceb02048060d578@ec2-23-21-171-249.compute-1.amazonaws.com:5432/d34sajf37v3dge");
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
